ebaf1ec3474edfb93eaa50f0bdb50df2ba41435a
Maciej Piotrowski <maccieju@gmail.com>
2023-08-06 14:14:22 -0400

    Some enhancements to the script

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install tmux zsh git
 
-chsh -s /bin/zsh
+sudo chsh $(whoami) -s $(which zsh)
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain beta
+curl https://sh.rustup.rs -sSf | sh -s -- --no-prompt --default-toolchain beta
